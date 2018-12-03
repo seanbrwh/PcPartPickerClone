68e03699423c97342bad4f04d350e7ff4fa25293
Sean White <sean.b.white89@icloud.com>
2018-12-03 10:54:14 -0700

    .

--- Server/controllers/Component_controller.js
+++ Server/controllers/Component_controller.js
@@ -1,7 +1,12 @@
 module.exports = {
   getCpu: async (req,res) => {      
-      let db = req.app.get('db');      
-      let cpu = await db.cpu.allcpu();
-      res.status(200).send(cpu);        
+      let db = req.app.get('db'); 
+      try {
+        let cpu = await db.cpu.allcpu();
+        res.status(200).send(cpu);        
+        
+      } catch (error) {
+        console.log(error)
+      }     
   }
 }
