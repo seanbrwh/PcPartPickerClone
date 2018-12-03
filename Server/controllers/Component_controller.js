26c953c559e45fe8d1ea2747aabc593787428ef2
Sean White <sean.b.white89@icloud.com>
2018-12-03 11:11:29 -0700

    .

--- Server/controllers/Component_controller.js
+++ Server/controllers/Component_controller.js
@@ -2,13 +2,8 @@ module.exports = {
   getCpu: async (req,res) => {      
       try {
         let db = req.app.get('DB');     
-        db.allcpu().then(response=>{
-          res.status(200).send(response);             
-        }).catch(err=>{
-          if(err){
-            console.log(err)
-          }
-        });    
+        const cpu = await db.allcpu()
+          res.status(200).send(cpu);                           
       } catch (error) {
         console.log(error)
       }
--- Server/index.js
+++ Server/index.js
@@ -1,21 +1,21 @@
 require('dotenv').config();
 const express = require('express'),
       bodyParser = require('body-parser'),
-      session = require('express-session'),
+      // session = require('express-session'),
       massive = require('massive'),
-      comp_crtl = require('./controllers/Component_controller')
-
-const {SERVER_PORT, CON_STRING} = process.env;
+      comp_crtl = require('./controllers/Component_controller');
 
 const app = express();
+const {SERVER_PORT, CON_STRING} = process.env;
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(session({
-  secret:'12p09s9s9df91',
-  resave:false,
-  saveUninitialized:false
-}))
+// app.use(session({
+//   secret:'12p09s9s9df91',
+//   resave:false,
+//   saveUninitialized:false
+// }))
 app.use( express.static( `${__dirname}/../build` ) )
 
 
