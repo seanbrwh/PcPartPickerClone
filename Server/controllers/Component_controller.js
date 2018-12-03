a50e8dd381c4323830c2908b5ff05ce48b0f51e1
Sean White <sean.b.white89@icloud.com>
2018-12-03 11:21:08 -0700

    test

--- Server/controllers/Component_controller.js
+++ Server/controllers/Component_controller.js
@@ -2,7 +2,9 @@ module.exports = {
   getCpu: async (req,res) => {      
       try {
         let db = req.app.get('DB');     
-        const cpu = await db.allcpu()
+        let testCpu = await db.allcpu()
+        console.log(testCpu)
+        const cpu = await db.allcpu();
           res.status(200).send(cpu);                           
       } catch (error) {
         console.log(error)
