7a9c893b5250b6b0202badee4ee1e3e0b1166445
Sean <sean.b.white89@icloud.com>
2018-12-03 17:31:45 -0700

    fixed home page

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -23,23 +23,24 @@ export default function Landing() {
             <button id='build-guide-btn'>View The Build Guides</button>
           </div>
           <div id='featured-build'>
-            FEATURED BUILD GUIDE
+            <h2>Featured Build Guide</h2> 
+            <div>
+              <h3>Modest AMD Gaming Build</h3>
+              <div>
+                <img src="" alt=""/>
+                <small>by manirelli</small>
+              </div>
+              <p>AMD Ryzen 5 2600</p>
+              <p>Parametric Video Card(Chipset: Radeon RX 580; Video Ram: 8GB; Length:223mm-403mm)</p>
+              <p>Cooler Master MasterBox Q300L MicroATX Mini Tower</p>
+              <h4>Pricing</h4>
+              <div>
+                <i className="fa fa-comment">43</i>
+                <i className="fa fa-arrow-up">26</i>
+              </div>
+            </div>
           </div>
           <div className="sample-build-guides">
-            <h1>Entry Level Intel Gaming Build</h1>
-            <p>
-              Intel Core i3-8100
-              Parametric Video Card (Chipset: Radeon RX 570)
-              Cougar MX330-G ATX Mid Tower
-            </p>
-            by ThoughtA <span><i className="fa fa-comment"> 36</i></span> <span><i className="fa fa-arrow-up">10</i></span> 
-            <h1>Entry Level Intel Gaming Build</h1>
-            <p>
-              Intel Core i3-8100
-              Parametric Video Card (Chipset: Radeon RX 570)
-              Cougar MX330-G ATX Mid Tower
-            </p>
-            by ThoughtA <span><i className="fa fa-comment"> 36</i></span> <span><i className="fa fa-arrow-up">10</i></span> 
             <h1>Entry Level Intel Gaming Build</h1>
             <p>
               Intel Core i3-8100
@@ -95,65 +96,6 @@ export default function Landing() {
               <i className="fa fa-comment">0</i><i className="fa fa-arrow-up">0</i>
             </div>
             <h2>First Born - Fantastic Mid Tier AMD <span>COST</span> Build (2018)</h2>
-            <div className='user-single'>
-              <img src={user_4} alt=""/>
-              <div className='user-content'>
-                <p>
-                  AMD Ryzen 5 2600
-                  GeForce GTX 1080
-                </p>
-                <small>by User</small>
-              </div>
-              <i className="fa fa-comment">0</i><i className="fa fa-arrow-up">0</i>
-            </div>
-            <h2>First Born - Fantastic Mid Tier AMD <span>COST</span> Build (2018)</h2>
-            <div className='user-single'>
-              <img src={user_4} alt=""/>
-              <div className='user-content'>
-                <p>
-                  AMD Ryzen 5 2600
-                  GeForce GTX 1080
-                </p>
-                <small>by User</small>
-              </div>
-              <i className="fa fa-comment">0</i><i className="fa fa-arrow-up">0</i>
-            </div>
-            <h2>First Born - Fantastic Mid Tier AMD <span>COST</span> Build (2018)</h2>
-            <div className='user-single'>
-              <img src={user_4} alt=""/>
-              <div className='user-content'>
-                <p>
-                  AMD Ryzen 5 2600
-                  GeForce GTX 1080
-                </p>
-                <small>by User</small>
-              </div>
-              <i className="fa fa-comment">0</i><i className="fa fa-arrow-up">0</i>
-            </div>
-            <h2>First Born - Fantastic Mid Tier AMD <span>COST</span> Build (2018)</h2>
-            <div className='user-single'>
-              <img src={user_4} alt=""/>
-              <div className='user-content'>
-                <p>
-                  AMD Ryzen 5 2600
-                  GeForce GTX 1080
-                </p>
-                <small>by User</small>
-              </div>
-              <i className="fa fa-comment">0</i><i className="fa fa-arrow-up">0</i>
-            </div>
-            <h2>First Born - Fantastic Mid Tier AMD <span>COST</span> Build (2018)</h2>
-            <div className='user-single'>
-              <img src={user_4} alt=""/>
-              <div className='user-content'>
-                <p>
-                  AMD Ryzen 5 2600
-                  GeForce GTX 1080
-                </p>
-                <small>by User</small>
-              </div>
-              <i className="fa fa-comment">0</i><i className="fa fa-arrow-up">0</i>
-            </div>
             <button>See All Completed Builds</button>
           </div>
         </div>
@@ -167,17 +109,41 @@ export default function Landing() {
             <div className='blog-row-1'>
               <div className="blog-item">
                 <img src={blog_3} alt=""/>
+                <p>
+                  charity: water Completion Report #10
+                  Posted Oct. 23, 2018 by philip
+                  Our tenth charity: water completion report arrived! Thanks to your support, we were able to fund water projects serving 887 people in Rwanda. More than half of the population of Rwanda lack access to clean and safe water. ...
+                </p>
+                <i className="fa fa-comment">1</i>
               </div>
               <div className="blog-item">
                 <img src={blog_2} alt=""/>
+                <p>
+                charity: water Completion Report #9
+                Posted Oct. 4, 2018 by philip
+                Our ninth charity: water completion report arrived! Thanks to your support, we were able to fund water projects serving 2,275 people in Ethiopia. ...
+                </p>
+                <i className="fa fa-comment">1</i>
               </div>
             </div>
             <div className='blog-row-2'>
               <div className="blog-item">
                 <img src={blog} alt=""/>
+                <p>
+                NVIDIA GeForce RTX 2080 Ti Benchmarks
+                Posted Sept. 19, 2018 by BoyScout
+                The NVIDIA GeForce RTX 2080 and 2080 Ti are officially available. MSI sent us their new GeForce RTX 2080 Ti DUKE to find out how the new RTX 2080 Ti compares to the previous generation cards.
+                </p>
+                <i className="fa fa-comment">1</i>
               </div>
               <div className="blog-item">
                 <img src={blog_4} alt=""/>
+                <p>
+                Fractal Design Meshify C Mini / X399 mATX Build
+                Posted March 30, 2018 by BoyScout
+                With ASRock's X399M Taichi motherboard ready to take Ryzen Threadripper to smaller spaces, we try to squeeze as much power as we can into an Fractal Design's new micro-ATX Meshify C Mini case. Check out our latest video to see how the build came together.
+                </p>
+                <i className="fa fa-comment">1</i>
               </div>
             </div>      
           </div>
